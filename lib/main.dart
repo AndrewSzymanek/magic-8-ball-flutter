ae34ccf0afff4838b8e1d1533d5e6bdaf04512c3
Andrew Szymanek <andrewszymanek@Andrews-MacBook-Pro.local>
2020-04-28 10:18:22 -0500

    added flat button, function for selecting random number to retrive random ball image

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue.shade800,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  void setBallNumber(){
+    ballNumber = Random().nextInt(4) + 1;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+
+    child: FlatButton(
+        onPressed: (){
+          setState(() {
+            setBallNumber();
+      });
+    },
+    child: Image.asset('images/ball$ballNumber.png'),
+     ),
+    );
+  }
+}
+
